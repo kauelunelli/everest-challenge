c9e6036544e8bbee2b1eb58833f852a3cbf457e1
dinobergue-viana <dinobergue.viana@warren.com.br>
2022-05-19 09:42:37 -0300

    refactor: altera nome dos componentes e muda nome da rota base

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import CreateUser from '../views/CreateUser.vue'
+import Home from '../views/UsersList.vue'
+import CreateUser from '../views/Register.vue'
 import UserDetails from '../views/UserDetails.vue'
 
 Vue.use(VueRouter)
@@ -9,7 +9,7 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'users',
     component: Home
   },
   {
